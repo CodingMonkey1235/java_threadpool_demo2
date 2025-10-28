bc89f4cd9ea91690865b695bcb26da580b506525
Alexander <>
2025-10-28 18:43:32 +0300

    Задание 3. Cобственный пул потоков. Исправил замечания

--- src/main/java/org/example/CustomThreadPool.java
+++ src/main/java/org/example/CustomThreadPool.java
@@ -1,32 +1,62 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Optional;
+
 public class CustomThreadPool {
-    private final BlockingQueue taskQueue;
-    private final CustomWorker[] workers;
-    static final Runnable STOP_SIGNAL = () -> { };
     private volatile boolean isRunning = true;
+    private final Object monitor;
+    private final CustomWorker[] workers;
+    private final LinkedList<Runnable> tasks;
+
+    public class CustomWorker extends Thread {
+
+        public CustomWorker(String name) {
+            super(name);
+        }
+
+        public void run() {
+            while (isRunning) {
+                Optional<Runnable> runnable;
+                synchronized (monitor) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            monitor.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Runnable task = tasks.getFirst();
+                    tasks.remove(task);
+                    runnable = Optional.of(task);
+                }
+                runnable.ifPresent(Runnable::run);
+            }
+        }
+    }
 
     public CustomThreadPool(int poolSize) {
-        taskQueue = new BlockingQueue();
+        monitor = new Object();
+        tasks = new LinkedList<>();
         workers = new CustomWorker[poolSize];
         for (int i = 0; i < poolSize; i++) {
-            workers[i] = new CustomWorker(taskQueue, "Worker-" + (i + 1));
+            workers[i] = new CustomWorker("Worker-" + (i + 1));
             workers[i].start();
         }
     }
 
     public void addTask(Runnable task) throws IllegalStateException {
-        if (isRunning) {
-            taskQueue.addTask(task);
-        } else {
-            throw new IllegalStateException("CustomThreadPool is not running");
+        synchronized (monitor) {
+            if (isRunning) {
+                tasks.add(task);
+                monitor.notifyAll();
+            } else {
+                throw new IllegalStateException("CustomThreadPool is not running");
+            }
         }
     }
 
     public void shutdown() {
         isRunning = false;
-        for (int i = 0; i < workers.length; i++) {
-            taskQueue.addTask(STOP_SIGNAL);
-        }
     }
 }
--- src/main/java/org/example/CustomThreadPoolDemo.java
+++ src/main/java/org/example/CustomThreadPoolDemo.java
@@ -7,6 +7,11 @@ public class CustomThreadPoolDemo {
         for (int i = 1; i <= 9; i++) {
             int taskId = i;
             pool.addTask(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
                 System.out.println("Executing Task " + taskId + " by " + Thread.currentThread().getName());
                 try {
                     Thread.sleep(1000);
@@ -16,19 +21,24 @@ public class CustomThreadPoolDemo {
             });
         }
 
-        pool.shutdown();
+        try {
+            Thread.sleep(5000);
+            pool.shutdown();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
 
         // пытаемся запустить после shutdown()
-        for (int i = 10; i <= 19; i++) {
-            int taskId = i;
-            pool.addTask(() -> {
-                System.out.println("Executing Task " + taskId + " by " + Thread.currentThread().getName());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            });
-        }
+//        for (int i = 10; i <= 19; i++) {
+//            int taskId = i;
+//            pool.addTask(() -> {
+//                System.out.println("Executing Task " + taskId + " by " + Thread.currentThread().getName());
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                }
+//            });
+//        }
     }
 }
